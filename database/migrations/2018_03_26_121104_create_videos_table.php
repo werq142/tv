4a1f51aceaa1011083ee9781cc2aaefc199efa5e
Victoria <victoria97916@gmail.com>
2018-03-26 15:56:43 +0300

    initial commit

---
+++ database/migrations/2018_03_26_121104_create_videos_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('video_name');
+            $table->integer('category_id')->unsigned();
+            $table->text('description');
+            $table->text('imdb');
+            $table->integer('age_control');
+            $table->date('date_created');
+            $table->string('video_logo');
+            $table->string('video_background');
+            $table->string('video_file');
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
